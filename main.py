b9520d55d52b06ce52993647da1af1b184204aa8
dasha_kon <62230044+dasha124@users.noreply.github.com>
2022-11-05 13:35:16 +0300

    L3-4

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-from lab_python_oop.rectangle import Rectangle
-from lab_python_oop.circle import Circle
-from lab_python_oop.square import Square
-import emoji
-
-
+from lab_python_fp.field import field, main1
+from lab_python_fp.gen_random import gen_random, main2
+from lab_python_fp.unique import Unique, main3
+from lab_python_fp.sort import result, main4
+from lab_python_fp.sort import result_with_lambda
+from lab_python_fp.print_result import test_1, test_2, test_3, test_4, main5
+from lab_python_fp.cm_timer import cm_timer_1, cm_timer_2, main6
+from lab_python_fp.process_data import f1, f2, f3, f4, main7
+import time
+import json
+import sys
 
 
 def main():
     print()
-    r = Rectangle("синего", 8, 8)
-    c = Circle("зеленого", 8)
-    s = Square("красного", 8)
-    print(r)
-    print(c)
-    print(s)
-    print("Работа с виртуальным окружением и pip")
 
-    print(emoji.emojize('Python is :thumbs_up:'))
+    #main1()
+    #main2()
+    #main3()
+    #main4()
+    #main5()
+    #main6()
+    main7()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
+
